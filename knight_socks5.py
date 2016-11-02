9da1f2cee0f522121a496330d6cc9c36c6edc0f2
b1eberg0n <biebergong@gmail.com>
2016-11-02 21:37:03 +0800

    func style

--- knight_socks5.py
+++ knight_socks5.py
@@ -6,25 +6,20 @@ from struct import unpack
 from gevent import socket, spawn
 from gevent.server import StreamServer
 
-def to_cli(cli, serv, serv_name):
-	try:
-		for buf in iter(lambda:serv.recv(1024*16), b''):
-			print(len(buf), 'from serv to cli')
-			cli.sendall(buf)
-	except OSError:
-		pass
-	cli.close()
-	serv.close()
-	print('server {} close'.format(serv_name))
+# def to_cli(cli, serv, serv_name):
+# 	try:
+# 		# for buf in iter(lambda:serv.recv(1024*16), b''):
+# 		# 	print(len(buf), 'from serv to cli')
+# 		# 	cli.sendall(buf)
+# 		[ cli.sendall(buf) for buf in iter(lambda:serv.recv(1024*16), b'') ]
+# 		cli.close()
+# 		serv.close()
+# 	except OSError:
+# 		pass
+# 	print('server {} close'.format(serv_name))
 
 
-def handle(conn, addr):
-	r = conn.recv(3)
-	if r == b'\x05\x01\x00':
-		conn.sendall(b'\x05\x00')
-	else:
-		return
-
+def get_dst(conn):
 	r = conn.recv(4)
 	if r[:3] == b'\x05\x01\x00':
 		if r[-1:] == b'\x01':
@@ -35,26 +30,73 @@ def handle(conn, addr):
 			dst = conn.recv(dst_length).decode()
 		else:
 			return
-		r = conn.recv(2)
-		port = unpack('>H', r)[0]
-		print(dst, port)
-		
-		s = socket.socket()
-		s.connect( (dst, port) )
-		g = spawn(to_cli, conn, s, dst)
-		g.start()
-		conn.sendall(b'\x05\x00\x00\x01\x00\x00\x00\x00\x10\x10')
-		try:
-			for buf in iter(lambda:conn.recv(1024*16), b''):
-				print(len(buf), 'from cli to serv ', buf)
-				s.sendall(buf)
-		except OSError:
-			pass
-		s.close()
-		conn.close()
-		g.kill()
+		return dst
 	else:
 		return
 
 
+def get_port(conn):
+	r = conn.recv(2)
+	port = unpack('>H', r)[0]
+	return port
+
+
+def get_dst_addr(conn):
+	r = conn.recv(3)
+	if r == b'\x05\x01\x00':
+		conn.sendall(b'\x05\x00')
+	else:
+		return
+
+	dst = get_dst(conn)
+	port = get_port(conn)
+	print(dst, port)
+	return (dst, port)
+
+
+def send(dst, buf, dst_type, dst_name):
+	print(len(buf), end=' ')
+	if dst_type == 'cli':
+		print('{} -> {}'.format('serv', 'cli'))
+	else:
+		print('{} -> {}'.format('cli', 'serv'))
+	dst.sendall(buf)
+
+
+def reply(dst, src, dst_type, dst_name):
+	try:
+		# for buf in iter(lambda:src.recv(1024*16), b''):
+		# 	dst.sendall(buf)
+		[ send(dst,buf,dst_type,dst_name) for buf in iter(lambda:src.recv(1024*16), b'') ]
+		dst.close()
+		src.close()
+	except OSError:
+		pass
+	print('{} close by {}'.format(dst_name, dst_type))
+	
+
+
+def reply_start(conn, dst_addr):
+	dst, port = dst_addr
+	s = socket.socket()
+	s.connect( (dst, port) )
+	conn.sendall(b'\x05\x00\x00\x01\x00\x00\x00\x00\x10\x10')
+	to_cli = spawn(reply, s, conn, 'serv', dst)
+	reply(conn, s, 'cli', dst_addr)
+	# s.close()
+	# conn.close()
+	to_cli.kill()
+
+
+def handle(conn, addr):
+	dst_addr = get_dst_addr(conn)
+	if dst_addr:
+		pass
+	else:
+		conn.close()
+		return
+
+	reply_start(conn, dst_addr)
+
+		
 StreamServer(('0.0.0.0', 1081), handle).serve_forever()
