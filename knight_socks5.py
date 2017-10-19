5e6c80109cf645a3cc036c52f94eff15e1791901
bjong <biebergong@gmail.com>
2017-10-19 15:14:27 +0800

    async

--- knight_socks5.py
+++ knight_socks5.py
@@ -2,101 +2,110 @@
 # copyright 2016 bjong
 
 from struct import unpack
+import asyncio
+import socket
 # import gevent
-from gevent import socket, spawn
-from gevent.server import StreamServer
-
-# def to_cli(cli, serv, serv_name):
-# 	try:
-# 		# for buf in iter(lambda:serv.recv(1024*16), b''):
-# 		# 	print(len(buf), 'from serv to cli')
-# 		# 	cli.sendall(buf)
-# 		[ cli.sendall(buf) for buf in iter(lambda:serv.recv(1024*16), b'') ]
-# 		cli.close()
-# 		serv.close()
-# 	except OSError:
-# 		pass
-# 	print('server {} close'.format(serv_name))
+# from gevent import socket, spawn
+# from gevent.server import StreamServer
 
 
-def get_dst(conn):
-	r = conn.recv(4)
-	if r[:3] == b'\x05\x01\x00':
-		if r[-1:] == b'\x01':
-			r = conn.recv(4)
-			dst = '.'.join([str(i) for i in unpack('BBBB', r)])
-		elif r[-1:] == b'\x03':
-			dst_length = unpack('B', conn.recv(1))[0]
-			dst = conn.recv(dst_length).decode()
-		else:
-			return
-		return dst
-	else:
-		return
+async def get_dst(reader_c):
+    r = await reader_c.read(4)
+    if r[:3] == b'\x05\x01\x00':
+        if r[-1:] == b'\x01':
+            r = await reader_c.read(4)
+            dst = '.'.join([str(i) for i in unpack('BBBB', r)])
+        elif r[-1:] == b'\x03':
+            dst_length_bin = await reader_c.read(1)
+            dst_length = unpack('B', dst_length_bin)[0]
+            dst_bin = await reader_c.read(dst_length)
+            dst = dst_bin.decode()
+        else:
+            return
+        return dst
+    else:
+        return
 
 
-def get_port(conn):
-	r = conn.recv(2)
-	port = unpack('>H', r)[0]
-	return port
+async def get_port(reader_c):
+    r = await reader_c.read(2)
+    port = unpack('>H', r)[0]
+    return port
 
 
-def get_dst_addr(conn):
-	r = conn.recv(3)
-	if r == b'\x05\x01\x00':
-		conn.sendall(b'\x05\x00')
-	else:
-		return
+async def get_dst_addr(reader_c, writer_c):
+    r = await reader_c.read(3)
+    if r == b'\x05\x01\x00':
+        writer_c.write(b'\x05\x00')
+    else:
+        return
 
-	dst = get_dst(conn)
-	port = get_port(conn)
-	print(dst, port)
-	return (dst, port)
-
-
-def send(dst, buf, dst_type, dst_name):
-	print(len(buf), end=' ')
-	if dst_type == 'cli':
-		print('{} -> {}'.format('serv', 'cli'))
-	else:
-		print('{} -> {}'.format('cli', 'serv'))
-	dst.sendall(buf)
+    dst = await get_dst(reader_c)
+    port = await get_port(reader_c)
+    # print(dst, port)
+    return (dst, port)
 
 
-def reply(dst, src, dst_type, dst_name):
-	try:
-		# for buf in iter(lambda:src.recv(1024*16), b''):
-		# 	dst.sendall(buf)
-		[ send(dst,buf,dst_type,dst_name) for buf in iter(lambda:src.recv(1024*16), b'') ]
-		dst.close()
-		src.close()
-	except OSError:
-		pass
-	print('{} close by {}'.format(dst_name, dst_type))
-	
+# def send(dst, buf, dst_type, dst_name):
+#     print(len(buf), end=' ')
+#     if dst_type == 'cli':
+#         print('{} -> {}'.format('serv', 'cli'))
+#     else:
+#         print('{} -> {}'.format('cli', 'serv'))
+#     dst.sendall(buf)
 
 
-def reply_start(conn, dst_addr):
-	dst, port = dst_addr
-	s = socket.socket()
-	s.connect( (dst, port) )
-	conn.sendall(b'\x05\x00\x00\x01\x00\x00\x00\x00\x10\x10')
-	to_cli = spawn(reply, s, conn, 'serv', dst)
-	reply(conn, s, 'cli', dst_addr)
-	# s.close()
-	# conn.close()
-	to_cli.kill()
+async def reply(reader, writer, dst_name):
+    # try:
+    #     [ send(dst,buf,dst_type,dst_name) for buf in iter(lambda:src.recv(1024*16), b'') ]
+    #     dst.close()
+    #     src.close()
+    # except OSError:
+    #     pass
+    while True:
+        buf = await reader.read(2048*4)
+        if not buf:
+            break
+        else:
+            # print(buf[:200])
+            writer.write(buf)
+            await writer.drain()
+    print('{} : close'.format(dst_name))
 
 
-def handle(conn, addr):
-	dst_addr = get_dst_addr(conn)
-	if dst_addr:
-		pass
-	else:
-		conn.close()
-		return
+async def reply_start(reader_c, writer_c, dst_addr):
+    dst, port = dst_addr
+    # print('reply', dst, port)
+    # dst_ip = await socket.getaddrinfo('ip.cn', port=80)[0][4][0]
+    reader_s, writer_s = await asyncio.open_connection(dst, port=port, loop=loop)
+    print('connect to', dst, port)
+    # s = socket.socket()
+    # s.connect( (dst, port) )
+    writer_c.write(b'\x05\x00\x00\x01\x00\x00\x00\x00\x10\x10')
+    await writer_s.drain()
+    loop.create_task(reply(reader_c, writer_s, dst))
+    await reply(reader_s, writer_c, dst_addr)
+    # to_cli.kill()
 
-	reply_start(conn, dst_addr)
 
-		
-StreamServer(('0.0.0.0', 1081), handle).serve_forever()
+async def handle(reader_c, writer_c):
+    # reader_s, writer_s = await asyncio.open_connection('ip.cn', 80, loop=loop)
+    # exit()
+
+
+    dst_addr = await get_dst_addr(reader_c, writer_c)
+    if dst_addr:
+        # print(dst_addr)
+        pass
+    else:
+        # reader_c.close()
+        return
+
+    await reply_start(reader_c, writer_c, dst_addr)
+
+
+ip, port = '127.0.0.1', 1080
+loop = asyncio.get_event_loop()
+core = asyncio.start_server(handle, ip, port, loop=loop)
+server = loop.run_until_complete(core)
+loop.run_forever()
